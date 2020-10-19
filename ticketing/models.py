fdfba654a1efddca926ae99dd4d06c9d478b307c
chong3916 <69540544+chong3916@users.noreply.github.com>
2020-10-19 16:33:17 -0500

    Added Databases

--- ticketing/models.py
+++ ticketing/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Ticket(models.Model):
+    issue_summary = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    issue_description = models.TextField()
+    date_submitted = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    target_resolution = models.DateTimeField(default=timezone.now)
+    priority = models.CharField(max_length=30)
+    issue_status = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.issue_summary
--- ticketing/views.py
+++ ticketing/views.py
@@ -1,35 +1,15 @@
 from django.shortcuts import render
-
-tickets = [
-    {
-        'submitter': 'Jane Doe',
-        'issue_id': '0231342342',
-        'issue_description': 'Test issue 1',
-        'date_identified': 'October 17, 2020',
-        'due_date': 'October 20, 2020',
-        'severity': 'major'
-    },
-    {
-        'submitter': 'John Doe',
-        'issue_id': '1234321223',
-        'issue_description': 'Test issue 2',
-        'date_identified': 'October 17, 2020',
-        'due_date': 'October 20, 2020',
-        'severity': 'minor'
-    }
-]
+from .models import Ticket
 
 
 def home(request):
-    context = {
-        'tickets': tickets
-    }
-    return render(request, 'ticketing/home.html', context)
+
+    return render(request, 'ticketing/home.html')
 
 
 def tickets(request):
     context = {
-        'title': 'Issues',
-        'tickets': tickets
+        'tickets': Ticket.objects.all(),
+        'title': 'Issues'
     }
     return render(request, 'ticketing/tickets.html', context)
