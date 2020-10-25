75e094934a2c93b3a737a1f9c1acf3f9bc31c1f2
chong3916 <69540544+chong3916@users.noreply.github.com>
2020-10-24 22:11:18 -0500

    Added Update User Profile Feature

--- ticketing/views.py
+++ ticketing/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from .models import Ticket
+from django.contrib.auth.decorators import login_required
 
-
+@login_required
 def home(request):
+    context = {
+        'tickets': Ticket.objects.all(),
+        'title': 'Dashboard'
+    }
+    return render(request, 'ticketing/home.html', context)
 
-    return render(request, 'ticketing/home.html')
-
-
+@login_required
 def tickets(request):
     context = {
         'tickets': Ticket.objects.all(),
